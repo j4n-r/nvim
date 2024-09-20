c368eb0a41291b56bb31f9f38d5d8aaedc02f152
Jan Rüggeberg <jan-rueggeberg@mirantus.com>
2024-09-20 11:00:44 +0200

    python lsp changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -98,8 +98,7 @@ return { -- LSP Configuration & Plugins
 		local servers = {
 			clangd = {},
 			gopls = {},
-			pyright = {},
-			-- rust_analyzer = {},
+			pylsp = {}, -- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
